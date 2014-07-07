d4d4eeccc72469093e811262f642a5901bb569fe
davidjfreedman <Winterfell@Davids-MacBook-Air.local>
2014-07-07 18:04:17 -0500

    BOOYAH

--- grnAvengers.js
+++ grnAvengers.js
@@ -24,31 +24,52 @@ var info = [{
     "bg": "http://a1.dspnimg.com/data/l/1848829587299_SiVhlrQ8_l.jpg"
 }, ]
 
+var hulk = 0
+
 $(function() {
-    var github_username = "graym";
-    var github_url = "https://api.github.com/users/";
-    var url = github_url + github_username;
+    function initApp() {
+        var github_username = info[hulk].username;
+        var github_url = "https://api.github.com/users/";
+        var url = github_url + github_username;
 
-    var github_data = $.getJSON(url);
+        var github_data = $.getJSON(url);
 
-    var script_tag = document.querySelector('#github_profile_template');
-    var template_string = script_tag.textContent;
-    var createProfileStringFromData = _.template(template_string);
+        var script_tag = document.querySelector('#github_profile_template');
+        var template_string = script_tag.textContent;
+        var createProfileStringFromData = _.template(template_string);
 
-    github_data.then(function(data) {
-        var github_html = createProfileStringFromData(data);
-        $('body').append(github_html);
+        github_data.then(function(data) {
+            var github_html = createProfileStringFromData(data);
+            $('.destination')[0].innerHTML = (github_html);
 
-        var leftArrow = $('.leftArrow');
-
-        function callback(event) {
-            console.log(event.type);
-        };
+            var leftArrow = $('.leftArrow');
+            var rightArrow = $('.rightArrow');
 
 
-        leftArrow[0].addEventListener("click", callback);
 
-    })
+            leftArrow[0].addEventListener("click", leftClick);
+            rightArrow[0].addEventListener("click", rightClick);
+
+        })
+    }
+
+    initApp();
+
+    function leftClick(event) {
+        hulk--;
+        if (hulk < 0) {
+            hulk = (info.length - 1);
+        }
+        initApp();
+    };
+
+    function rightClick(event) {
+        hulk++;
+        if (hulk == info.length) {
+            hulk = 0;
+        }
+        initApp();
+    };
 
 
 });
